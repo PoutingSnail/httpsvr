342391bb493e386816479ca33a62f902e9820a09
httpsvr <93423415@qq.com>
2024-04-17 09:00:55 +0800

    basic function for http service

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/healthz", healthz)
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func healthz(res http.ResponseWriter, req *http.Request) {
+	if req != nil && req.Header != nil {
+		for key, value := range req.Header {
+			fmt.Printf("key: %s, value: %s \n", key, value)
+			res.Header().Set(key, value)
+		}
+	}
+	res.WriteHeader(403)
+	io.WriteString(res, "ok")
 }
